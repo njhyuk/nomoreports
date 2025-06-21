992c2ebcb55f9da1e3bfa76bb33edfd987292624
njhyuk <njh2239@gmail.com>
2025-06-21 09:59:10 +0900

    first commit

---
+++ src/main/kotlin/com/njhyuk/nomoreports/NomoreportsApplication.kt
@@ -0,0 +1,11 @@
+package com.njhyuk.nomoreports
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NomoreportsApplication
+
+fun main(args: Array<String>) {
+	runApplication<NomoreportsApplication>(*args)
+}
---
+++ src/test/kotlin/com/njhyuk/nomoreports/NomoreportsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.njhyuk.nomoreports
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NomoreportsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
